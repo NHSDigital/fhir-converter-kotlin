135038c53dda928e58574252753a8e16e2644c63
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-01-31 15:32:56 +0000

    AMB-1007 add MedicationStatement support

--- src/test/kotlin/net/nhsd/fhir/converter/ResourceTypeInferenceTest.kt
+++ src/test/kotlin/net/nhsd/fhir/converter/ResourceTypeInferenceTest.kt
@@ -7,12 +7,16 @@ import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.hl7.fhir.dstu3.model.MedicationRequest as R3MedicationRequest
+import org.hl7.fhir.dstu3.model.MedicationStatement as R3MedicationStatement
 import org.hl7.fhir.r4.model.MedicationRequest as R4MedicationRequest
+import org.hl7.fhir.r4.model.MedicationStatement as R4MedicationStatement
 
 internal class ResourceTypeInferenceTest {
     companion object {
         private const val MEDICATION_REQUEST_JSON = "{\"resourceType\":  \"MedicationRequest\"}"
         private const val MEDICATION_REQUEST_XML = "<MedicationRequest></MedicationRequest>"
+        private const val MEDICATION_STATEMENT_JSON = "{\"resourceType\":  \"MedicationStatement\"}"
+        private const val MEDICATION_STATEMENT_XML = "<MedicationStatement></MedicationStatement>"
 
         private val JSON = MediaType.APPLICATION_JSON
         private val XML = MediaType.APPLICATION_XML
@@ -67,5 +71,41 @@ internal class ResourceTypeInferenceTest {
 
         // Then
         assertThat(resource).isEqualTo(R3MedicationRequest::class.java)
+    }
+
+    @Test
+    fun `it should return r4 medication statement given json input`() {
+        // Given
+        val resource = getResourceType(MEDICATION_STATEMENT_JSON, JSON, R4)
+
+        // Then
+        assertThat(resource).isEqualTo(R4MedicationStatement::class.java)
+    }
+
+    @Test
+    fun `it should return r4 medication statement given xml input`() {
+        // Given
+        val resource = getResourceType(MEDICATION_STATEMENT_XML, XML, R4)
+
+        // Then
+        assertThat(resource).isEqualTo(R4MedicationStatement::class.java)
+    }
+
+    @Test
+    fun `it should return r3 medication statement given json input`() {
+        // Given
+        val resource = getResourceType(MEDICATION_STATEMENT_JSON, JSON, DSTU3)
+
+        // Then
+        assertThat(resource).isEqualTo(R3MedicationStatement::class.java)
+    }
+
+    @Test
+    fun `it should return r3 medication statement given xml input`() {
+        // Given
+        val resource = getResourceType(MEDICATION_STATEMENT_XML, XML, DSTU3)
+
+        // Then
+        assertThat(resource).isEqualTo(R3MedicationStatement::class.java)
     }
 }
